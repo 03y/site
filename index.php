1262bbeb041a184683b48774654636914d3bc986
03y <98747393+03y@users.noreply.github.com>
2022-05-11 18:15:00 +0100

    can change colour scheme

--- index.php
+++ index.php
@@ -3,9 +3,29 @@
 <head>
 <title>moir.org.uk</title>
 <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
+<script>
+$(document).ready(function () {
+    var toggle = true;
+    $("body").on({
+        dblclick: function () {
+            if (toggle) {
+                $(this).css("background-color", "dcdccc");
+                $(this).css("color", "black");
+                toggle = false;
+            } else {
+                $(this).css("background-color", "1e1e1e");
+                $(this).css("color", "white");
+                toggle = true;
+            }
+        }
+    });
+});
+</script>
+
 <h1>moir.org.uk</h1>
 <?php
 $dir = array_diff(scandir('/opt/lampp/htdocs/'), array('.', '..', 'style.css', 'index.php'));
